3893a591e7be2ec23f6e17f0a3812d1fbf37fe6d
ahmadreza tahmasbi <75021820+ahmadrezatmb@users.noreply.github.com>
2021-06-23 11:22:02 +0430

    deleted

--- cut/admin.py
+++
@@ -1,9 +0,0 @@
-from cut.models import urlsdatabase
-from django.contrib import admin
-from .models import urlsdatabase
-# Register your models here.
-
-class urlsregister(admin.ModelAdmin):
-    pass
-
-admin.site.register(urlsdatabase , urlsregister)
--- cut/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from django.forms import widgets
-
-class userurl(forms.Form):
-    address = forms.CharField(label='' , widget= forms.TextInput(attrs={'class':'addressbar'}))
